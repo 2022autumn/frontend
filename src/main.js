9e46f09c86d01416ff116040a1df6e0b95b8a8d1
zbh6666 <bohao2020@163.com>
2022-10-27 08:50:08 +0800

    [impl]:添加vuetify

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import vuetify from './plugins/vuetify'
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false
@@ -11,5 +12,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
