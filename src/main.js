67de1c6679672b239e801b25ff12ee150d5af773
zbh6666 <bohao2020@163.com>
2022-11-21 01:27:05 +0800

    添加词云组件

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import * as echarts from 'echarts';
+Vue.prototype.$echarts = echarts
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import vuetify from './plugins/vuetify'
