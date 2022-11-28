771d196beae87df7954fa2174b843ed0ccf10de9
ZhMX1 <zhmxchina@163.com>
2022-11-28 16:25:19 +0800

    zmx 1128-1

--- src/components/paperitem.vue
+++ src/components/paperitem.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="item">
-    <div style="margin-top: 10px">
+    <div style="margin-top: 2vh">
       <el-row>
         <el-col :span="2">
           <div class="item-type">期刊</div>
@@ -24,8 +24,7 @@
       <span style="margin-left: 10px; margin-right: 10px">发表时间</span>
     </div>
     <div class="item-content">
-      新冠肺炎疫情的出现是个"黑天鹅"事件.通过对疫情影响下的总供给
-      ,总需求及CPI的测算,并综合自然走势和政策效果分析,预计2020年的中国经济增速为5.5%-6.0%,
+      新冠肺炎疫情的出现是个"黑天鹅"事件.通过对疫情影响下的总供给,总需求及CPI的测算,并综合自然走势和政策效果分析,预计2020年的中国经济增速为5.5%-6.0%,
       CPI上涨率能够控制在4.0%以内.疫情对中国经济自然走势的负面影响很大,2020年增增速目标的扩...
     </div>
     <div style="width: 100%">
@@ -66,53 +65,16 @@
     </div>
   </div>
 </template>
+
 <style>
 /* 下方中间推荐内容 */
-.reference {
-  font-family: "Inter";
-  font-style: normal;
-  display: inline-block;
-  text-align: center;
-  width: 100%;
-  margin-bottom: 20px;
-}
-.button {
-  margin: auto;
-  width: 8vw;
-  height: 6vh;
-  background: #217bf4;
-  box-shadow: 0px 7px 22px -6px rgba(0, 72, 168, 0.34);
-  border-radius: 14px;
-
-  font-weight: 700;
-  font-size: 20px;
-  line-height: 50px;
-  text-align: center;
-  letter-spacing: 0.01em;
-
-  color: #ffffff;
-}
-.ref-tag {
-  width: 50%;
-  height: 40px;
-  margin-top: 5px;
-  background: rgba(117, 167, 235, 0.1);
-  color: rgb(47, 44, 74);
-  border-radius: 14px;
-
-  font-weight: 700;
-  font-size: 20px;
-  line-height: 40px;
-  text-align: center;
-  letter-spacing: 0.01em;
-}
 .item {
   margin: auto;
   margin-left: 3%;
   margin-bottom: 4vh;
   padding: 1%;
   width: 95%;
-  height: 28vh;
+  height: 30vh;
 
   background: linear-gradient(250.39deg, #ffffff 10.13%, #ffffff 94%);
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
