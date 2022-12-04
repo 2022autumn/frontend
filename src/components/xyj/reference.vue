8fe659b7eaf1cddebc1427a2f209e48b529a802f
jeffiny <1223160472@qq.com>
2022-12-04 23:09:04 +0800

    相关文献，引用文献修改

--- src/components/xyj/reference.vue
+++ src/components/xyj/reference.vue
@@ -2,7 +2,7 @@
   <div class="re-total">
     <div class="title">引用文献</div>
     <div class="title-en">Reference</div>
-    <div class="ref-box-set">
+    <div class="ref-box-set" v-infinite-scroll="load" v-if="have_refer===true">
       <div class="ref-box" v-for="(item,index) in refer" :key="index">
         <div class="index-box">
           <div class="index">{{ index+1 }}</div>
@@ -10,6 +10,9 @@
         <div class="content">{{ item.cited }}</div>
         <div class="line"></div>
       </div>
+    </div>
+    <div class="null" v-if="have_refer===false">
+      无相关引用文献
     </div>
   </div>
 </template>
@@ -35,12 +38,17 @@ export default {
     }).then(response => {
           console.log(response.data);
           this.refer = response.data.data.referenced_works;
+          if(this.refer===null){
+            this.have_refer=false;
+          }
+          console.log(this.have_refer)
         }
     )
   },
   data(){
     return{
       refer_id:[],
+      have_refer:true,
       refer:["P.J. Schultz and K.G. Lynn, Rev. Mod. Phys., vol. 60, pp. 701, 1988.",
               "H. Utsunomiya et al., \"Phys. Rev. C67 (2003) 015807; H. Utsunomiya\", in Proc. of Int. Workshop on Laser Photons at SPring-8, pp. 89, 2000.",
             "D.A. Gryaznykh, V.A. Lykov and V.V Plokhoi, Nucl. Instr. and Meth., vol. A448, pp. 106, 2000.",
@@ -56,12 +64,12 @@ export default {
 <style scoped>
 .re-total{
   position: absolute;
-  width: 900px;
-  height: auto;
-  overflow-y: auto;
+  width: 920px;
+  height: 400px;
   background: #FFFFFF;
   box-shadow: -2px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 5px;
+  margin-left:20px;
 }
 .title{
   position: absolute;
@@ -84,7 +92,7 @@ export default {
   position: absolute;
   width: 84px;
   height: 26px;
-  left: 800px;
+  left: 790px;
   top: 32px;
 
   font-family: 'Poppins';
@@ -102,9 +110,25 @@ export default {
   position: relative;
   display: flex;
   flex-direction: column;
-  width: 600px;
-  top:50px;
+  width: 98%;
+  margin-top:80px;
   margin-bottom: 110px;
+  overflow-y:scroll;
+  overflow-x: hidden;
+  height: 265px;
+  padding-bottom: 20px;
+}
+::-webkit-scrollbar {
+
+  width: 7px;
+  height: 18px;
+  border-radius: 8px;
+}
+::-webkit-scrollbar-thumb {
+  width: 7px;
+  height: 20px;
+  background-color: #E8E8E8;
+  border-radius: 8px;
 }
 .ref-box{
   position: relative;
@@ -116,7 +140,8 @@ export default {
   height: 29px;
   left: 50px;
   top: 50px;
-
+  display: flex;
+  justify-content: center;
   background: #E7F2FD;
   border-radius: 4px;
 
@@ -124,10 +149,10 @@ export default {
 .index{
   display: flex;
   position: relative;
+  justify-content: center;
   width: 9px;
   height: 26px;
   top:1px;
-  left: 10px;
   font-family: 'Inter';
   font-style: normal;
   font-weight: 600;
@@ -140,7 +165,7 @@ export default {
 }
 .content{
   display: flex;
-  width: 730px;
+  width: 750px;
   margin-left: 90px;
   padding-left:17px;
   margin-top:50px;
@@ -157,5 +182,17 @@ export default {
 
   border: 1.4px solid rgba(217, 215, 215, 0.58);
   transform: rotate(0.09deg);
+}
+.null{
+  position: relative;
+  height:20px;
+  bottom: 10px;
+  color: #0E84F4;
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 40px;
+  top:150px;
+  left: 300px;
 }
 </style>
